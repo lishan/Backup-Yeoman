778b3eee70b27050ee51e687f14f56396b6b0ede
Fred <lishan.yu@oracle.com>
2014-02-26 16:21:09 +0800

    init project, add basic functions to access web application

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.get('/hello.txt', function(req, res){
+  var body = '<html><head><title>this is a title</title></head><body><h1>I\'m a test</h1></body></html>';
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(body));
+  res.end(body);
+});
+app.listen(3000);
+console.log('Listening on port 3000');
