092c11f20a474422b5e573a0390cbc93749222fa
Fred <lishan.yu@oracle.com>
2014-03-05 11:59:45 +0800

    create layout header and footer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,19 +3,24 @@
  * Date: 2/28/14
  * Time: 11:02 PM
  * Require js load module, include all the front end javascript like jquery and angular
+ * @Deprecated cause by require js support angular issue
  */
 require.config({
-    baseUrl : "/bower_components",
     paths : {
-        jquery : "jquery/jquery.min",
-        angular : "angular/angular.min",
-        bootstrap : "bootstrap/docs/assets/js/bootstrap.min"
+        'jquery' : "/bower_components/jquery/dist/jquery.min",
+        'angular' : "/bower_components/angular/angular.min",
+        'angular-route' : "/bower_components/angular-route/angular-route.min",
+        'bootstrap' : "/bower_components/bootstrap/dist/js/bootstrap.min",
+        'test' : "./test"
+    },
+    shim : {
+        'bootstrap' : {deps : ['jquery']},
+        'angular' : {exports : 'angular'},
+        'angular-route' : {deps : ['angular']},
+        'test' : {deps: ['angular','angular-route']}
     }
-})
-require(['jquery',
-    'angular',
-    'bootstrap',
-    '/javascripts/test.js'
-], function(){
-    console.log("Require js load files completed.");
+});
+
+require(['jquery','bootstrap','angular','angular-route'],function(){
+    console.log('Require js load module completed.');
 });
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -3,12 +3,13 @@
  * Date: 3/3/14
  * Time: 11:12 AM
  */
-var app = angular.module("myApp",[]);
-app.controller("a",['$scope','$http',function($scope,$http){
-    var p = $http({
+var app = angular.module('myApp', ['ngRoute']);
+app.controller("a", ['$scope', '$http', function ($scope, $http) {
+    $http({
         method: 'GET',
         url: '/'
-    }).success(function(res,status,headers,config){
+    }).success(function (res, status, headers, config) {
             $scope.model = res["user"];
         });
 }]);
+
