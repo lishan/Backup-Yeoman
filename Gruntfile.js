88e597677ff459d8c7d52fd33433b19c38a5dc2b
LishanYu <yulishan3@gmail.com>
2014-03-24 14:28:08 +0800

    gruntfile update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,17 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['routes/*.js'],
+                dest: 'build/built.js'
+            }
+        }
     });
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', []);
 }
